1b52261f0bcdd8ee5c7033d7599bb701c52f5245
RaRoiz <tataphon32@gmail.com>
2024-11-27 19:04:16 +0700

    init

--- server/src/configs/tls.config.ts
+++ server/src/configs/tls.config.ts
@@ -3,9 +3,9 @@ import { file } from "bun"
 let _tls = {}
 const mode = Bun.env.MODE || 'production'
 
-if(mode != 'production') {
+if(mode !== 'production') {
     const cert = file("../ssl/localhost.pem")
-    const key = file("../ssl/localhost-key.pem")
+    const key = file("..sll/localhost-key.pem")
     _tls = {cert,key}
 }
 
