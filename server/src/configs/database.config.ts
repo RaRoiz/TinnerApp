f5316465f7b18f022b70bb8e600b7c71495c3523
RaRoiz <tataphon32@gmail.com>
2024-12-18 08:06:10 +0700

    init

--- server/src/configs/database.config.ts
+++ server/src/configs/database.config.ts
@@ -5,8 +5,7 @@ const username = Bun.env.MONGO_GO_USER|| 'tatapholph'
 const db_password = Bun.env.MONGO_GO_PASS|| 'yourpassword'
 const db_name = Bun.env.MONGO_DBNAME ||'tinnerapp'
 
-const uri = `mongodb+srv://${username}:${db_password}@cluster0.uladk.mongodb.net/?retryWrites=true&w=majority&appName=${db_name}`
-                                        //APGT5uYPzlhPJejk
+const uri = `mongodb+srv://${username}:APGT5uYPzlhPJejk@cluster0.uladk.mongodb.net/?retryWrites=true&w=majority&appName=${db_name}`
 export const MongoDB = {
     connect:async function (){
         try {
